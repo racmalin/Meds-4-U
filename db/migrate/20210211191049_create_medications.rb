457a2570d4d91a70a3bab5957c3262f72f314d81
racmalin <racmalin@yahoo.com>
2021-02-12 11:18:08 -0700

    rollback AR to version 5.2

--- db/migrate/20210211191049_create_medications.rb
+++ db/migrate/20210211191049_create_medications.rb
@@ -1,4 +1,4 @@
-class CreateMedications < ActiveRecord::Migration[6.1]
+class CreateMedications < ActiveRecord::Migration[5.2]
   def change
     create_table :medications do |t|
       t.string :name
