3d31cfc6310b5b3fa48e0c69a87fd3ab21d5cdee
racmalin <racmalin@yahoo.com>
2021-02-26 12:52:42 -0700

    adding routes and CRUD

--- app/controller/medications_controller.rb
+++ app/controller/medications_controller.rb
@@ -23,4 +23,25 @@ class MedicationsController < ApplicationController
         @medications.users << User.find_by_id(params[:user_id].to_i)
         redirect "/medications"
     end
+    
+    private
+
+    get '/medications/:id/edit' do
+      @medication = Medication.find_by(id: params[:id])
+      erb :"medications/edit"
+    end
+
+    patch '/medications/:id' do
+        medication = Medication.find_by(id: params[:id])
+        medication.update(name: params[:name], description: params[:description], 
+        disease_states_name: params[:disease_states_name], brand: params[:brand], dose: params[:dose], quantity: params[:quantity])
+        redirect "/medications/#{medication.id}"
+    end
+
+    delete '/medications/:id' do
+        medication = Medication.find_by(id: params[:id])
+        medication.delete
+        redirect "/medications"
+    end
+
 end
