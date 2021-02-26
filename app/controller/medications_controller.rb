ab040e729d817f9a4c690eb54b3a193671ebbdf3
jordon_terry <jt5493@hotmail.com>
2021-02-26 13:56:35 +0900

    fixed medication routes

--- app/controller/medications_controller.rb
+++ app/controller/medications_controller.rb
@@ -4,8 +4,8 @@ class MedicationsController < ApplicationController
       
         require_login
         user = User.find_by_id(session[:user_id])
-        # @medications = user.medications
-        @medications = Medication.all
+        @medications = user.medications
+        # @medications = Medication.all
         erb :"/medications/index"   
       end
 
@@ -16,16 +16,16 @@ class MedicationsController < ApplicationController
     end
 
     post "/medications" do    
-        @name = params[:name]
+        
         @medications = Medication.create(name: params[:name], description: params[:description], 
-        disease_states: params[:disease_states], 
-        medication_id: params[:medication_id], user_id: params[:user_id])
-      
+        disease_states_name: params[:disease_states_name], 
+        medication_id: params[:medication_id])
+        @medications.users << User.find_by_id(params[:user_id].to_i)
+        redirect "/medications"
     end
 
-    post "/medications" do
-      erb :"medications/show"
-    end
+
+
         # if @medication.create
         #     redirect "/medications"
         # else
