e0f32219e2f26b6f43f1101aaaedc5059ee630bb
racmalin <racmalin@yahoo.com>
2021-02-27 00:25:12 -0700

    finishing up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 doxil = Medication.create!(name: 'Doxil')
 bob = User.create!(username: 'Bob', password_digest: 'password')
 cancer = DiseaseState.create!(name: 'cancer', disease_type: 'carcinoma', category: 'fatal', user_id: bob.id, medication_id: doxil.id)
